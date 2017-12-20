8a1715ad8aa89696f2fdd46ddd78f3bc3cedcc2a
mark miranda <notmarkmiranda@gmail.com>
2017-12-19 20:00:15 -0700

    Add user model and validations

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  context 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+  end
+
+  context 'relationships'
+  context 'methods'
+end
