5583f626120a374783f1401d3238c36a942e1bf2
mark miranda <notmarkmiranda@gmail.com>
2017-12-20 07:48:54 -0700

    Create token and timestamp migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220025133) do
+ActiveRecord::Schema.define(version: 20171220144420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20171220025133) do
     t.string "password_digest"
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "token"
+    t.index ["token"], name: "index_users_on_token", unique: true
   end
 
 end
